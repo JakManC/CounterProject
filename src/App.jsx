9c0c41973c97a028d33010bd0e7bc7ca7e45f03b
JakManC <mangeshgchaudhari75@gmail.com>
2025-11-23 17:31:58 +0530

    Initial File
    ReactInitialFile

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1
+        className="
+      bg-orange-700 text-white p-2 m-2 text-center hover:bg-pink-700  rounded-full 
+      "
+      >
+        This Is Counter Project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
